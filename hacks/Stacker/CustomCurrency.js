92266f7ac10e95780167f2475573dc2a71056819
jake461 <66709889+jake461@users.noreply.github.com>
2020-06-10 04:22:19 -0400

    Update CustomCurrency.js
    y

--- hacks/Stacker/CustomCurrency.js
+++ hacks/Stacker/CustomCurrency.js
@@ -1,30 +1,105 @@
-const setCurrency = (id, value) => {
-    let config = "currency";
-    let x = hack.player.backpack.data[config]; // shorten things
-    let i = x.findIndex(v => v.ID === id); // find index if it exists
-    
-    if (i === -1) x.push({N: value, ID: id}) // if it doesn't, push
-    else x[i] = {N: value, ID: id} // if it does, modify it
-}
-
-/** Possible config values:
- * boots: []
- * currency: []
- * follow: []
- * fossil: []
- * hat: []
- * item: []
- * key: []
- * mount: []
- * outfit: []
- * relic: []
- * spellRelic: []
- * weapon: []
- **/
+Skip to content
  
- /** Usage of setCurrency(id, value)
-  * setCurrency(1, 999);
-  * setCurrency(5, 420);
-  * setCurrency(16, 42);
-  * setCurrency(4, 10000);
-  **/
+Search or jump to…
+
+Pull requests
+Issues
+Marketplace
+Explore
+ 
+@jake461 
+Learn Git and GitHub without any code!
+Using the Hello World guide, you’ll start a branch, write comments, and open a pull request.
+
+ 
+jake461
+/
+ProdigyMathGameHacking
+forked from Prodigy-Hacking/ProdigyMathGameHacking
+0
+0 88
+ Code
+ Pull requests 0  Actions
+ Projects 0
+ Wiki
+ Security 0
+ Insights
+ Settings
+ProdigyMathGameHacking/.github/workflows/prodigy mod
+@jake461 jake461 prodigy mod
+442a847 now
+61 lines (60 sloc)  1.09 KB
+  
+// Sets your nickname.
+hack.player.name.data.nickname = 7; 
+
+/* Put a number one through 55.  The associations are as follows:
+ * 1.  Buccaneer
+ * 2.  Captain
+ * 3.  First-Mate
+ * 4.  Beard the Pirate
+ * 5.  Treasure Hunter
+ * 6.  Robo
+ * 7.  The W12  - 4R D
+ * 8.  Dancemaster
+ * 9.  Bot 30 00 
+ * 10. The Machinist
+ * 11. The Digger
+ * 12. Surveyor
+ * 13. Excavator
+ * 14. Archaeologist
+ * 15. Saurus
+ * 16. Pilot
+ * 17. The Skyfolk
+ * 18. Cloudeater
+ * 19. Stormchaser
+ * 20. , Puyoy!
+ * 21. Firelord
+ * 22. Flame Wielder
+ * 23. Pyromancer
+ * 24. Coalwalker
+ * 25. The Hottie
+ * 26. Of the Forest
+ * 27. Botanist
+ * 28. The Green
+ * 29. Forest Walker
+ * 30. Wild
+ * 31. Ice Carver
+ * 32. Frozen
+ * 33. The Snowman
+ * 34. Mountaineer Climber
+ * 35. The Cool
+ * 36. The Firedancer
+ * 37. Woodland
+ * 38. The Wild
+ * 39. Snapdragon
+ * 40. The Firefly
+ * 41. Chef
+ * 42. Hotshot
+ * 43. Firebreather
+ * 44. Flame Dancer
+ * 45. Bolt-Head
+ * 46. The Mechanic
+ * 47. Super
+ * 48. Chill
+ * 49. Mountaineer
+ * 50. Frostbringer
+ * 51. Aquatic
+ * 52. Fishy
+ * 53. Of the Sea
+ * 54. Mer
+ * 55. Glub-Glub
+ * null. Original Name
+ */
+© 2020 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Help
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
